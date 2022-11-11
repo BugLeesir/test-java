a824d0cedd5b242839ee69de8a45a7233808dae9
BugLeesir <1007878154@qq.com>
2022-11-12 01:17:48 +0800

    jaab

--- Student.java
+++ Student.java
@@ -18,10 +18,7 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
-    @Override
-    public String toString() {
-        return "Student [name=" + name + ", age=" + age + "]";
-    }
+   
    
     
     
