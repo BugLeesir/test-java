222c0a18b2fe0d453228522b4b75a329dc330270
BugLeesir <1007878154@qq.com>
2022-11-12 01:57:19 +0800

    ppp

--- Student.java
+++ Student.java
@@ -18,6 +18,10 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", age=" + age + "]";
+    }
   
    
     
