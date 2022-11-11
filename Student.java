faaf9ae0990e1fdfb4c87c6bbf9efbb4127d4883
BugLeesir <1007878154@qq.com>
2022-11-12 01:26:30 +0800

    袖肥

--- Student.java
+++ Student.java
@@ -20,7 +20,7 @@ public class Student {
     }
     @Override
     public String toString() {
-        return "Student [name=" + name + ", age=" + age + "]"+"fdjfs";
+        return "Student [name=" + name + ", age=" + age + "]";
         
     }
    
